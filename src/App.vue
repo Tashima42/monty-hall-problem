4bda11759706204bdf3c97045331aac67d0ec5b5
Pedro Tashima <pedrotashima42@gmail.com>
2020-07-30 23:42:39 -0300

    Added style and logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Monty Hall Problem</h1>
+    <div class="form">
+      <div v-if="!started">
+        <label for="doorsAmount">Quantas Portas?</label>
+        <input type="text" id="doorsAmount" size="3" v-model.number="doorsAmount" />
+      </div>
+      <div v-if="!started">
+        <label for="selectedDoor">Qual porta é premiada?</label>
+        <input type="text" id="selectedDoor" size="3" v-model.number="selectedDoor" />
+      </div>
+      <button v-if="!started" @click="started = true">Iniciar</button>
+      <button v-if="started" @click="started =false">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+        <div v-for="i in doorsAmount" :key="i">
+            <Door :hasGift="i == selectedDoor" :numeber="i" />
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Door from "./components/Door";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Door },
+  data: function () {
+    return {
+      started: false,
+      doorsAmount: 3,
+      selectedDoor: null,
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  font-family: monospace, sans-serif;
+}
+
+body {
+  color: #fff;
+  background: #feac5e; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #4bc0c8,
+    #c779d0,
+    #feac5e
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #4bc0c8,
+    #c779d0,
+    #feac5e
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
+.doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin-right: 10px;
 }
 </style>
